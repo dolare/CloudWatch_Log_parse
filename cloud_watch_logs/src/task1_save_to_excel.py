882d2b8f88220ced7a648f2fd9814518661bdbdf
dolarebrave@gmail.com <dolarebrave@gmail.com>
2017-11-02 11:08:19 -0400

    add debug exception

--- cloud_watch_logs/src/task1_save_to_excel.py
+++ cloud_watch_logs/src/task1_save_to_excel.py
@@ -14,15 +14,31 @@ if __name__ == "__main__":
     clean_folder(os.path.dirname(__file__) + '/tmp')
 
     #export log to s3 and return taskid
-    export_task = export(int(sys.argv[1]), int(sys.argv[2]))
+
+    try:
+        export_task = export(int(sys.argv[1]), int(sys.argv[2]))
+    except:
+        print('export fail')
+
 
     #wait for trasfering and download log gz files from s3 bucket
     time.sleep(3)
-    file_names = download(export_task)
+
+    try:
+        file_names = download(export_task)
+    except:
+        print('download fail')
 
     #parse the data and return data as json(dict) format
-    data = parse_for_excel(file_names)
+    try:
+        data = parse_for_excel(file_names)
+    except:
+        print('parse fail')
+
     time.sleep(3)
 
-    #save data as excel 
-    save_as_excel(data)
+    #save data as excel
+    try:
+        save_as_excel(data)
+    except:
+        print('save file fail')
